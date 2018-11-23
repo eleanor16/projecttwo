1d6f235a10bfdcae847b463b1ef7c6c5443688b0
Hannah Olsen <holsen@hawk.iit.edu>
2018-11-22 20:53:19 -0600

    Add javascript to toggle nav open and closed

--- site.js
+++ site.js
@@ -7,8 +7,25 @@
 
     console.log('The DOM has loaded');
 
+    var html_tag = document.querySelector('html');
+    html_tag.classList.remove('nojs');
 
+    html_tag.classList.add('js');
+    var heading_label = 'Menu';
+    var heading = documnet.querySelector('nav');
+    heading.innerHTML = '<a href="#menu">'+heading_label+'</a>';
+    heading.addEventListener('click',function(){
+      var content = document.querySelector('#menu');
+      var label = document.querySelector('nav a');
 
+      content.classList.toggle('visible');
+
+      if (label.textContent === heading_label) {
+        label.textContent = 'Menu';
+      } else {
+        label.textContent = 'Hide Menu';
+      }
+    })
   });
 
   console.log("JavaScript is working!");

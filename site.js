0576a08d0b861dfc74ad77a290c1640df0346b57
Hannah Olsen <holsen@hawk.iit.edu>
2018-11-22 22:28:12 -0600

    Add toggle part of javascript for nav

--- site.js
+++ site.js
@@ -1,5 +1,8 @@
+console.log('top is here');
 (function(){
+  console.log('funtion ran');
   if (typeof document.querySelector === 'undefined') {
+  console.log("JavaScript is working!");
     return;
   }
 
@@ -8,26 +11,34 @@
     console.log('The DOM has loaded');
 
     var html_tag = document.querySelector('html');
+    console.log('12 is good');
     html_tag.classList.remove('nojs');
-
+    console.log('14 is good');
     html_tag.classList.add('js');
+    console.log('16 is good');
     var heading_label = 'Menu';
-    var heading = documnet.querySelector('nav');
-    heading.innerHTML = '<a href="#menu">'+heading_label+'</a>';
-    heading.addEventListener('click',function(){
+    console.log('18 is good');
+    var heading = document.querySelector('#nav');
+    console.log('20 is good');
+   heading.innerHTML = '<a href="#menu">'+heading_label+'</a>';
+   console.log('22 is good');
+    heading.addEventListener('click', function(){
+      console.log('line 24');
       var content = document.querySelector('#menu');
-      var label = document.querySelector('nav a');
-
+      console.log('25 is good');
+      var label = document.querySelector('#nav a');
+      console.log('27 is good');
       content.classList.toggle('visible');
-
+      console.log('29 is good');
       if (label.textContent === heading_label) {
-        label.textContent = 'Menu';
-      } else {
         label.textContent = 'Hide Menu';
+        console.log('32 is good');
+      } else {
+        label.textContent = 'Menu';
+        console.log('35 is good');
       }
     })
   });
 
   console.log("JavaScript is working!");
-
-});
+})();

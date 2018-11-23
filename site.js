373e7a78b8ae4a8e69cf8702f66529ee2bee7910
Hannah Olsen <holsen@hawk.iit.edu>
2018-11-22 18:57:23 -0600

    Add basic loading functions to javascript file

---
+++ site.js
@@ -0,0 +1,16 @@
+(function(){
+  if (typeof document.querySelector === 'undefined') {
+    return;
+  }
+
+  document.addEventListener('DOMContentLoaded', function(){
+
+    console.log('The DOM has loaded');
+
+
+
+  });
+
+  console.log("JavaScript is working!");
+
+});

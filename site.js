44eb8fa39d9d585e1a5c48f92231c4327f0c5ef5
Hannah Olsen <holsen@hawk.iit.edu>
2018-12-05 22:55:59 -0600

    Delete old functions from js file

--- site.js
+++ site.js
@@ -1,7 +1,6 @@
 //based off code from Karl Stolley at https://github.com/sbwd-2018/dom-scripting/blob/master/site.js
 (function(){
   if (typeof document.querySelector === 'undefined' || typeof window.getComputedStyle === 'undefined') {
-  console.log("JavaScript is working!");
     return;
   }
 
@@ -49,55 +48,6 @@
       html_tag.classList = 'js';
     }
   }
-
-
-/*  function navToggle(){
-    var html_tag = document.querySelector('html');
-    console.log('12 is good');
-    html_tag.classList.remove('nojs');
-    console.log('14 is good');
-    html_tag.classList.add('js');
-    console.log('16 is good');
-    var heading_label = 'Menu';
-    console.log('18 is good');
-    var heading = document.querySelector('#nav');
-    console.log('20 is good');
-   heading.innerHTML = '<a href="#menu">'+heading_label+'</a>';
-   console.log('22 is good');
-    heading.addEventListener('click', function(){
-      console.log('line 24');
-      var content = document.querySelector('#menu');
-      console.log('25 is good');
-      var label = document.querySelector('#nav a');
-      console.log('27 is good');
-      content.classList.toggle('visible');
-      console.log('29 is good');
-      console.log("hwllo" + label.textContent);
-      if (label.textContent === heading_label) {
-        label.textContent = 'Hide Menu';
-        console.log('32 is good');
-      } else {
-        label.textContent = 'Menu';
-        console.log('35 is good');
-      }
-    })
-
-
-    if (typeof window.matchMedia === 'undefined') {
-      return;
-    }
-
-    if (window.matchMedia("(min-width: 426px)"). matches) {
-        html_tag.classList = 'nojs';
-    } else {
-      html_tag.classList = 'js';
-    }
-
-  }
-
-*/
-
-
 
 
   console.log("JavaScript is working!");
